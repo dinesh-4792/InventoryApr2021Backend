57653481dfe6e0e296645108811ef6e5608d2d48
Dinesh Gunasekaran <dinesh4792@gmail.com>
2021-04-24 15:25:11 +0800

    1st Commit

---
+++ routes/products.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const asyncHandler = require('express-async-handler');
+const firebase = require('../services/firebase');
+const collectionId = "product";
+
+router.get('/',asyncHandler(async(req,res,next) => {
+    //Business Logic
+    const products = await firebase.getCollection(collectionId)
+    // console.log(products)
+    return res.json({
+        products : products
+    })
+}));    
+
+router.post('/new',asyncHandler(async(req,res,next) =>{
+    //Business Logic
+    try{
+        const payload = req.body; // Get request body
+        const documentId = payload.productName;
+        const status = await firebase.setDocument(collectionId, documentId, payload)
+        return res.json({
+            status:status
+        });
+    }catch(error){
+        console.error(error);
+        return res.json({
+            status:500
+        });
+    }
+}));
+
+module.exports = router;
+
