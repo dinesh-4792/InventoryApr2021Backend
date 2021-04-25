77e017f913458d8255d0873b037e38b25d7a0808
Dinesh Gunasekaran <dinesh4792@gmail.com>
2021-04-25 14:56:00 +0800

    25 Apr

--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,22 @@ const asyncHandler = require('express-async-handler');
 const firebase = require('../services/firebase');
 const collectionId = "product";
 
+module.exports = router;
+
+router.use(asyncHandler(async function(req,res,next){
+    const headers = req.headers;
+    try{
+        if(!firebase.verifyIdToken(headers.idtoken, headers.uid)){
+            return res.json({status:"Access is prohibited"})
+        }
+        next();
+    }
+    catch(err){
+        console.error('[Users API Middleware]: ${err}')
+        return res.json({status:"Access is prohibited"})
+    }
+}));
+
 router.get('/',asyncHandler(async(req,res,next) => {
     //Business Logic
     const products = await firebase.getCollection(collectionId)
@@ -30,5 +46,5 @@ router.post('/new',asyncHandler(async(req,res,next) =>{
     }
 }));
 
-module.exports = router;
+
 
